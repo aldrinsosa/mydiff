72a89eced20250f2d4230edbd799a4410d5ee1cd
Aldrin Sosa <sosaadrin2006@gmail.com>
2025-10-28 14:54:27 +0100

    get the length of the LCS

--- mydiff/Program.cs
+++ mydiff/Program.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace mydiff
 {
-    class  Program
+    class Program
     {
         public static void Main(string[] args)
         {
@@ -12,13 +12,14 @@ namespace mydiff
                 Console.WriteLine("Usage: mydiff <first_file> <second_file>");
                 return;
             }
+
             //set address of the files
             string path = Directory.GetCurrentDirectory();
             string arg1 = args[0];
             string arg2 = args[1];
             IEnumerable<string> firstFileE;
             IEnumerable<string> secondFileE;
-            
+
             //read the files
             try
             {
@@ -29,6 +30,7 @@ namespace mydiff
                 Console.WriteLine(e.Message);
                 return;
             }
+
             try
             {
                 secondFileE = File.ReadLines($"{path}/{arg2}");
@@ -39,22 +41,55 @@ namespace mydiff
                 return;
             }
 
-            int firstFileEnumCount = firstFileE.Count();
-            int  secondFileEnumCount = secondFileE.Count();
-            Line[,] lines = new Line[firstFileEnumCount,secondFileEnumCount];
-            Line [] linesFirst =  new Line[firstFileEnumCount];
-            Line [] linesSecond = new Line[secondFileEnumCount];
+            //create the matrix for the LCS
+            int firstFileCount = firstFileE.Count() + 1; // + 1 is for the empty string
+            int secondFileCount = secondFileE.Count() + 1;
+            int[,] lengthLCS = new Int32[firstFileCount, secondFileCount];
+            Line[] linesFirst = new Line[firstFileCount];
+            Line[] linesSecond = new Line[secondFileCount];
             int idx = 0;
+            linesFirst[idx] = new Line() { NumberLine = idx++, ContentLine = "" };
             foreach (string line in firstFileE)
             {
-                Line l = new Line() {NumberLine = idx + 1, ContentLine = line};
+                Line l = new Line() { NumberLine = idx + 1, ContentLine = line };
                 linesFirst[idx++] = l;
             }
+
             idx = 0;
+            linesSecond[idx] = new Line() { NumberLine = idx++, ContentLine = "" };
             foreach (string line in secondFileE)
             {
-                Line l = new Line() {NumberLine = idx + 1, ContentLine = line};
+                Line l = new Line() { NumberLine = idx + 1, ContentLine = line };
                 linesSecond[idx++] = l;
+            }
+
+            for (int i = 0; i < firstFileCount; i++)
+            {
+                for (int j = 0; j < secondFileCount; j++)
+                {
+                    if (j == 0 || i == 0)
+                    {
+                        //the LCS for the empty string is always 0 
+                        lengthLCS[i, j] = 0;
+                    }
+                    else if (linesFirst[i].ContentLine.Equals(linesSecond[j].ContentLine))
+                    {
+                        //if the content matches get the upper-left value and increase it by 1
+                        lengthLCS[i, j] = lengthLCS[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        //if the content don't match get the max value between the left and upper value
+                        if (lengthLCS[i, j - 1] > lengthLCS[i - 1, j])
+                        {
+                            lengthLCS[i, j] = lengthLCS[i, j - 1];
+                        }
+                        else
+                        {
+                            lengthLCS[i, j] = lengthLCS[i - 1, j];
+                        }
+                    }
+                }
             }
         }
     }
